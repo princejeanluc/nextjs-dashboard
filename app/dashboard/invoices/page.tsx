a536f3b8dd098d20776ee8ff51467d6d18c9b070
princejeanluc <princejeanluc.denteppe@gmail.com>
2024-02-11 01:33:28 +0100

    adding features

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import {fetchInvoicesPages} from "@/app/lib/data";
 
 export default async function Page({searchParams,}:{
     searchParams?:{
@@ -14,6 +15,7 @@ export default async function Page({searchParams,}:{
 }) {
     const query =  searchParams?.query|| ''
     const currentPage = Number(searchParams?.page)||1
+    const totalPages = await fetchInvoicesPages(query)
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -27,7 +29,7 @@ export default async function Page({searchParams,}:{
                 <Table query={query} currentPage={currentPage} />
              </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                 <Pagination totalPages={totalPages} />
             </div>
         </div>
     );
